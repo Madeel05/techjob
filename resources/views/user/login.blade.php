6f967bb9edbf909ab498faa4830df0a2f530bb32
Muhammad Adeel <76046496+Madeel05@users.noreply.github.com>
2023-12-25 14:33:21 +0500

    Login form design

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-lg">
+                    <div class="card-header">Login</div>
+                    <form action="" method="post" autocomplete="off">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="">Email</label>
+                                <input type="email" name="email" class="form-control">
+                                @if($errors->has('email'))
+                                    <div class="text-danger">{{$errors->first('email')}}</div>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control">
+                                @if($errors->has('password'))
+                                    <div class="text-danger">{{$errors->first('password')}}</div>
+                                @endif
+                            </div>
+                            <br/>
+                            <div class="form-group text-center">
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
