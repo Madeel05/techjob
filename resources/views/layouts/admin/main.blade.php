68b20f8ffd5adff5b3c8eaca4a4b2b61cb87b311
Muhammad Adeel <76046496+Madeel05@users.noreply.github.com>
2024-01-15 21:51:21 +0500

    integrate admin theme

---
+++ resources/views/layouts/admin/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.admin.header')
+<body class="sb-nav-fixed">
+@include('layouts.admin.navbar')
+<div id="layoutSidenav">
+    @include('layouts.admin.sidebar')
+    <div id="layoutSidenav_content">
+        @include('layouts.admin.body')
+        @include('layouts.admin.footer')
+    </div>
+</div>
+@include('layouts.admin.scripts')
+</body>
+</html>
